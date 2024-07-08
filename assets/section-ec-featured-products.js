8e97d4d8f23ba10a05472340d4f42bc71c8724ca
Arif Hossin || Full Stack Developer <webdeveloperarif@gmail.com>
2024-07-09 00:18:41 +0600

    Update

---
+++ assets/section-ec-featured-products.js
@@ -0,0 +1,39 @@
+// class ECFeaturedProduct extends HTMLElement {
+//   constructor() {
+//     super();
+//   }
+
+//   connectedCallback() {
+//     this.productPointer = this.querySelector('.ec-featured-product__item-pointer');
+//     if (this.productPointer) {
+//       this.productPointer.addEventListener('click', this.toggleModal.bind(this));
+//     }
+//   }
+
+//   disconnectedCallback() {
+//     if (this.productPointer) {
+//       this.productPointer.removeEventListener('click', this.toggleModal.bind(this));
+//     }
+//   }
+
+//   toggleModal(event) {
+//     alert("CLICKED");
+//     const targetModalId = event.currentTarget.getAttribute('data-target');
+//     const targetModal = document.getElementById(targetModalId);
+//     if (targetModal) {
+//       targetModal.classList.toggle('active');
+//     }
+//   }
+// }
+// customElements.define('ec-featured-product', ECFeaturedProduct);
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   document.querySelectorAll('.ec-modal-close').forEach(function (closeButton) {
+//     closeButton.addEventListener('click', function (event) {
+//       const modal = event.currentTarget.closest('.ec-modal');
+//       if (modal) {
+//         modal.classList.remove('active');
+//       }
+//     });
+//   });
+// });
